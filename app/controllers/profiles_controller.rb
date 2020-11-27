4e5287b7492faab2294c392311a3f50171d61d66
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-27 20:59:31 +0900

    Update profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,16 @@ class ProfilesController < ApplicationController
   def new
   end
 
+  def create
+    @profile = Profile.new(profile_params)
+  end
+
   def edit
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:contact, :team_id).merge(user_id: current_user.id)
   end
 end
