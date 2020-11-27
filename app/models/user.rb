6cd3983d4d1613a4b311350071512f114a6143fa
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-27 19:30:18 +0900

    userモデルのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  has_one :profile
+  has_many :evaluations
+  has_many :progresses
+  has_many :reviews
+  end
