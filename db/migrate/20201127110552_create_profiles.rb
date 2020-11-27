3a9f11dbc99a766259638fc9a8d09818e17ec6d4
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-27 20:11:37 +0900

    profileモデル カラム追加

--- db/migrate/20201127110552_create_profiles.rb
+++ db/migrate/20201127110552_create_profiles.rb
@@ -1,6 +1,9 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
+      t.string :contact, null: false
+      t.references :user, null: false, foreign_key: true
+      t.integer :team_id, null: false
       t.timestamps
     end
   end
