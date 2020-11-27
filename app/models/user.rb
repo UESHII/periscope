d9087aa848b31d766d43cfe8b34aada034e9fd6c
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-27 19:31:04 +0900

    userモデル、マイグレーションファイルのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   has_many :evaluations
   has_many :progresses
   has_many :reviews
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  
+  with_options presence: true do
+    validates :name, format: { with: NAME_REGEX, message: "is invalid. Input full-width characters."}
+    validates :email
+    validates :password, format: { with: PASSWORD_REGEX, message: "is invalid. Input 6 characters at least, use both of letter and number."}
   end
