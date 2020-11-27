c51e83733c3402d633644199e5d5aed341b074bf
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-27 20:36:17 +0900

    Active Hash(Team)の設定

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,11 @@
 class Team < ApplicationRecord
+  self.data = [
+    { id: 1, name: 'Acconting' },
+    { id: 2, name: 'Customer Service' },
+    { id: 3, name: 'General' },
+    { id: 4, name: 'IT' },
+    { id: 5, name: 'Logistics' },
+    { id: 6, name: 'Manufacturing' },
+    { id: 7, name: 'Sales' }
+  ]
 end
