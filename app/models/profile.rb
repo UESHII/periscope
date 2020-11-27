09106ee7f8856617394db9e8d91605387c616f78
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-27 20:40:08 +0900

    profileモデル active hashの設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,11 @@
 class Profile < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to_active_hash :team
 
   with_options :presence true do
     validates :user
     validates :team
+    validates :team_id
   end
 end
