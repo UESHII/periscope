60741845332ede82ebe9c66f6cdccd74c017b1d9
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-11-28 12:27:19 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'explores/index'
-  root to: "profiles#new"
+  root to: "profiles#index"
   devise_for :users
   resources :profiles, only: [:index, :new, :create, :edit]
 end
