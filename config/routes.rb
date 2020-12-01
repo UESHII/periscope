faf164d2c8cadde569bf44f06372a6b0932cd67e
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-12-02 08:38:20 +0900

    topからexploreへのリンク

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "profiles#index"
   devise_for :users
   resources :profiles, only: [:index, :new, :create, :edit, :show]
+  resources :explores, only: [:index]
 end
