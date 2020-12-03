5548098f38ba947626940849d447c365f4661031
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-12-03 19:29:34 +0900

    アソシエーションとバリデーションの追加

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,2 +1,11 @@
 class Evaluation < ApplicationRecord
+  belongs_to :user
+  has_many :progresses
+  has_many :reviews
+
+  with_options presence: true do
+    validates :goal
+    validates :fiscal_year
+    validates :user
+  end
 end
