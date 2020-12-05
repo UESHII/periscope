0168aa44bec90981643df6bb48ef1f8649c59751
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-12-05 16:49:44 +0900

    devise registration controllerのオーバーライドを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
     set_flash_message :notice, :destroyed
     yield resource if block_given?
-    redirect_to root_path
+    redirect_to new_user_session_path
   end
 end
