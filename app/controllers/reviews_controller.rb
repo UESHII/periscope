3825a7a2e78a375f0909bcdaa1432861d5f166bb
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2020-12-05 10:28:44 +0900

    レビュー投稿後の遷移方法修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,20 +1,13 @@
 class ReviewsController < ApplicationController
-  def new
-    @review = Review.new
-  end
-
+  
   def create 
-    @review = Review.new(review_params)
-    if @review.save
-      redirect_to "/evaluations/#{review.evaluation.id}"
-    else
-      render action: :new
-    end
+    @review = Review.create(review_params)
+    redirect_to "/evaluations/#{@review.evaluation.id}"
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:goal, :dark).merge(user_id: current_user.id, evaluation_id: params[:evaluation_id])
+    params.require(:review).permit(:clear, :dark).merge(user_id: current_user.id, evaluation_id: params[:evaluation_id])
   end
 end
