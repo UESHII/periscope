e1bd616ee40d90822e32f06c284580b45bf9a6be
Takahiro Ueshima <takahiro.ueshima@gmail.com>
2021-01-06 14:12:02 +0900

    Evaluation投稿の結合テスト

---
+++ spec/system/evaluations_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+require 'rails_helper'
+
+RSpec.describe "Evaluation", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @profile = FactoryBot.create(:profile, user_id: @user.id)
+    @evaluation = FactoryBot.build(:evaluation)
+  end
+  context "投稿できるとき" do
+    it "正しい操作で投稿できる" do
+      # ログインページへ遷移
+      visit root_path
+      # パスを確認
+      expect(current_path).to eq new_user_session_path
+      # 各項目に入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      # 送信する
+      click_on 'ログイン'
+      # 送信後の遷移先がトップページであることを確認
+      expect(current_path).to eq root_path
+      # NEW POSTをクリックしてEvaluation投稿画面へ遷移
+      click_on 'NEW POST'
+      # 遷移先パスを確認
+      expect(current_path).to eq '/evaluations/new'
+      # 各項目を入力する
+      find("option[value='2020']").select_option
+      # find("option[value='12").select_option
+      select(value = "12", from: "evaluation[fiscal_year(2i)]") 
+      fill_in "evaluation[goal]", with: "example"
+      # 送信するとプロフィールのデータカウントが1増えることを確認
+      expect{
+        find('input[class="submit"]').click
+      }.to change { Evaluation.count }.by(1)
+      # 送信後の遷移パスがトップページであることを確認
+      expect(current_path).to eq root_path
+    end
+  end
+  context "投稿できないとき" do
+    it "誤った操作で投稿できない場合新規投稿画面が再度表示される" do
+      # ログインページへ遷移
+      visit root_path
+      # パスを確認
+      expect(current_path).to eq new_user_session_path
+      # 各項目に入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      # 送信する
+      click_on 'ログイン'
+      # 送信後の遷移先がトップページであることを確認
+      expect(current_path).to eq root_path
+      # NEW POSTをクリックしてEvaluation投稿画面へ遷移
+      click_on 'NEW POST'
+      # 遷移先パスを確認
+      expect(current_path).to eq '/evaluations/new'
+      # 各項目を入力する
+      # find("option[value='2020']").select_option
+      # find("option[value='12").select_option
+      # select(value = "12", from: "evaluation[fiscal_year(2i)]") 
+      # fill_in "evaluation[goal]", with: "example"
+      # 送信するとプロフィールのデータカウントが1増えることを確認
+      expect{
+        find('input[class="submit"]').click
+      }.to change { Evaluation.count }.by(0)
+      # 送信後の遷移パスがトップページであることを確認
+      expect(current_path).to eq '/evaluations'
+      # エラーメッセージが表示されていることを確認
+      expect(page).to have_content("翌期の目標は入力必須です。")
+    end
+  end
+end
+
